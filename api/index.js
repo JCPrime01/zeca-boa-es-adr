5db4b04a77cf2f2543cd5b52339f51e1e616b7c4
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-16 09:29:05 -0300

    chore: update shareCode to NA8KGP4C9ZM

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=AWCSVCY8SXI&afp=bd1' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=NA8KGP4C9ZM&afp=bd1' });
     res.end();
   }
