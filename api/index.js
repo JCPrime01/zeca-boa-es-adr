22da7bd6b7c945867fb4182ca14df15ba814b2fd
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-19 09:52:04 -0300

    chore: update shareCode to 7JDZ45NO2FN

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=66NGGVV0N0B&afp=bd1' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=7JDZ45NO2FN&afp=bd1' });
     res.end();
   }
